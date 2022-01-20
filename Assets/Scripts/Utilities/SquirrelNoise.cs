df39d0fcaed593666b40d38e418cb4889b309be8
fishtaco567 <tgamer27@gmail.com>
2022-01-20 02:13:21 -0600

    initial commit

---
+++ Assets/Scripts/Utilities/SquirrelNoise.cs
@@ -0,0 +1,101 @@
+﻿/// <summary>
+/// Hash-based "noise" function implemented by Squirrel Eiseroh's GDC talk "Noise-Based RNG"
+/// Convenience functions are provided to allow calls with up to 4 coordinates, and retrieve values in different ranges
+/// </summary>
+public static class SquirrelNoise {
+
+    /// <summary>
+    /// Fast 1D Noise Function
+    /// </summary>
+    /// <param name="position">Position along the noise stream for the given seed. Provided as an int for convenience, is reinterpreted as uint</param>
+    /// <param name="seed">Seed of the noise stream, this should not change</param>
+    /// <returns>Random value from 0 to 2^32 - 1</returns>
+    public static uint Get1DNoiseUint(int position, uint seed = 0) {
+        const uint BIT_NOISE1 = 0x68E31DA4;
+        const uint BIT_NOISE2 = 0x85297A4D;
+        const uint BIT_NOISE3 = 0x1B56C4E9;
+
+        unchecked {
+            uint mangledBits = (uint) position;
+            mangledBits *= BIT_NOISE1;
+            mangledBits += seed;
+            mangledBits ^= (mangledBits >> 8);
+            mangledBits += BIT_NOISE2;
+            mangledBits ^= (mangledBits << 8);
+            mangledBits *= BIT_NOISE3;
+            mangledBits ^= (mangledBits >> 8);
+
+            return mangledBits;
+        }
+    }
+
+    /// <summary>
+    /// Fast 2D Noise Function for convinience, combines X and Y
+    /// </summary>
+    /// <returns>Random value from 0 to 2^32 - 1</returns>
+    public static uint Get2DNoiseUint(int posX, int posY, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 27742151, seed));
+    }
+
+    /// <summary>
+    /// Fast 3D Noise Function for convinience, combines X and Y
+    /// </summary>
+    /// <returns>Random value from 0 to 2^32 - 1</returns>
+    public static uint Get3DNoiseUint(int posX, int posY, int posZ, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 27833021 + posZ * 317130731, seed));
+    }
+
+    /// <summary>
+    /// Fast 4D Noise Function for convinience, combines X and Y
+    /// </summary>
+    /// <returns>Random value from 0 to 2^32 - 1</returns>
+    public static uint Get4DNoiseUint(int posX, int posY, int posZ, int posW, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 29399999 + posZ * 325767523 + posW * 1495052261, seed));
+    }
+
+    public static float Get1DNoiseZeroToOne(int position, uint seed = 0) {
+        return unchecked((Get1DNoiseUint(position, seed) - 1f) / (uint.MaxValue));
+    }
+
+    public static float Get2DNoiseZeroToOne(int posX, int posY, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 27742151, seed) / (uint.MaxValue + 1f));
+    }
+
+    public static float Get3DNoiseZeroToOne(int posX, int posY, int posZ, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 27833021 + posZ * 317130731, seed) / (uint.MaxValue + 1f));
+    }
+
+    public static float Get4DNoiseZeroToOne(int posX, int posY, int posZ, int posW, uint seed = 0) {
+        return unchecked(Get1DNoiseUint(posX + posY * 29399999 + posZ * 325767523 + posW * 1495052261, seed) / (uint.MaxValue + 1f));
+    }
+    public static float Get1DNoiseNegativeOneToOne(int position, uint seed = 0) {
+        return unchecked((Get1DNoiseUint(position, seed) / (float) uint.MaxValue) * 2 - 1);
+    }
+
+    public static float Get2DNoiseNegativeToOne(int posX, int posY, uint seed = 0) {
+        return unchecked((Get1DNoiseUint(posX + posY * 27742151, seed) / (float)uint.MaxValue) * 2 - 1);
+    }
+
+    public static float Get3DNoiseNegativeToOne(int posX, int posY, int posZ, uint seed = 0) {
+        return unchecked((Get1DNoiseUint(posX + posY * 27833021 + posZ * 317130731, seed) / (float)uint.MaxValue) * 2 - 1);
+    }
+
+    public static float Get4DNoiseNegativeToOne(int posX, int posY, int posZ, int posW, uint seed = 0) {
+        return unchecked((Get1DNoiseUint(posX + posY * 29399999 + posZ * 325767523 + posW * 1495052261, seed) / (float)uint.MaxValue) * 2 - 1);
+    }
+
+    public static int Get1DNoiseInt(int position, uint seed = 0) {
+        return unchecked((int) Get1DNoiseUint(position, seed));
+    }
+
+    public static int Get2DNoiseInt(int posX, int posY, uint seed = 0) {
+        return unchecked((int)Get1DNoiseUint(posX + posY * 27742151, seed));
+    }
+    public static int Get3DNoiseInt(int posX, int posY, int posZ, uint seed = 0) {
+        return unchecked((int)Get1DNoiseUint(posX + posY * 27833021 + posZ * 317130731, seed));
+    }
+    public static int Get4DNoiseInt(int posX, int posY, int posZ, int posW, uint seed = 0) {
+        return unchecked((int)Get1DNoiseUint(posX + posY * 29399999 + posZ * 325767523 + posW * 1495052261, seed));
+    }
+
+}
---
+++ Assets/Scripts/Utilities/SquirrelRandom.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Custom random class build on Squirrel Eiselroh's noise-based RNG
+/// </summary>
+public class SRandom {
+
+    private uint seed;
+    private int position;
+
+    /// <summary>
+    /// Initialize a new SRandom
+    /// </summary>
+    /// <param name="seed">Seed from which to generate the random stream</param>
+    /// <param name="position">Initial position along the stream</param>
+    public SRandom(uint seed, int position = 0) {
+        this.seed = seed;
+        this.position = position;
+    }
+
+    /// <summary>
+    /// Reset the random to another seed
+    /// </summary>
+    /// <param name="seed">New seed</param>
+    /// <param name="position">New position</param>
+    public void ResetSeed(uint seed, int position = 0) {
+        this.seed = seed;
+        this.position = position;
+    }
+
+    /// <returns>The seed of this random generator</returns>
+    public uint GetSeed() {
+        return seed;
+    }
+
+    /// <summary>
+    /// Sets the current position of the random generator. This can be used to rewind or fast-forward random sequences
+    /// </summary>
+    /// <param name="position">The new position</param>
+    public void SetCurrentPosition(int position) {
+        this.position = position;
+    }
+
+    /// <returns>The current position of the random generator</returns>
+    public int GetCurrentPosition() {
+        return position;
+    }
+
+    /// <summary>
+    /// Generates next random uint
+    /// </summary>
+    /// <returns>Random integer from 0 - 2^32 - 1</returns>
+    public uint RandomUInt() {
+        return SquirrelNoise.Get1DNoiseUint(position++, seed);
+    }
+
+    /// <summary>
+    /// Generates next random int
+    /// </summary>
+    /// <returns>Random integer from -2^31 - 2^31 - 1</returns>
+    public int RandomInt() {
+        return unchecked((int) SquirrelNoise.Get1DNoiseUint(position++, seed));
+    }
+
+    /// <summary>
+    /// Generates next random integer less than a given value (exclusive)
+    /// </summary>
+    /// <param name="lessThan">The upper limit of the random integer (exclusive)</param>
+    /// <returns>A random integer from 0 - (lessThan - 1)</returns>
+    public int RandomIntLessThan(int lessThan) {
+        if(lessThan == 0) {
+            return 0;
+        }
+        int thing = (int)Math.Floor(SquirrelNoise.Get1DNoiseZeroToOne(position++, seed) * lessThan);
+        return thing % lessThan;
+    }
+
+    /// <summary>
+    /// Generates next random integer between a lower limit (inclusive) and an upper limit (inclusive)
+    /// </summary>
+    /// <param name="lowerInclusive">The lower limit (inclusive)</param>
+    /// <param name="upperInclusive">The upper limit (inclusive)</param>
+    /// <returns>Random integer between -lowerInclusive - upperInclusive</returns>
+    public int RandomIntInRange(int lowerInclusive, int upperInclusive) {
+        return (int)Math.Floor(SquirrelNoise.Get1DNoiseZeroToOne(position++, seed) * (upperInclusive - lowerInclusive + 1) + lowerInclusive);
+    }
+
+    /// <returns>Random number from 0 - 1</returns>
+    public float RandomFloatZeroToOne() {
+        return SquirrelNoise.Get1DNoiseZeroToOne(position++, seed);
+    }
+
+    /// <returns>Random number from -1 - 1</returns>
+    public float RandomFloatNegativeOneToOne() {
+        return SquirrelNoise.Get1DNoiseNegativeOneToOne(position++, seed);
+    }
+
+    /// <summary>
+    /// Generates next random float between a lower limit (inclusive) and an upper limit (inclusive)
+    /// </summary>
+    /// <param name="lowerInclusive">The lower limit (inclusive)</param>
+    /// <param name="upperInclusive">The upper limit (inclusive)</param>
+    /// <returns></returns>
+    public float RandomFloatInRange(float lowerInclusive, float upperInclusive) {
+        return (SquirrelNoise.Get1DNoiseNegativeOneToOne(position++, seed) + 1) * ((upperInclusive - lowerInclusive) / 2) + lowerInclusive;
+    }
+
+    /// <summary>
+    /// Rolls random chance for a given probability
+    /// </summary>
+    /// <param name="probabilityTrue">The probability that the roll is true</param>
+    /// <returns>The outcome of the roll (true for success)</returns>
+    public bool RandomChance(float probabilityTrue) {
+        return SquirrelNoise.Get1DNoiseZeroToOne(position++, seed) < probabilityTrue;
+    }
+
+    /// <summary>
+    /// Generates next random direction evenly distributed, with trig functions
+    /// </summary>
+    /// <returns>A random direction</returns>
+    public Vector2 RandomDirection2D() {
+        var theta = RandomFloatInRange(0, 2 * (float) Math.PI);
+        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
+    }
+
+    /// <summary>
+    /// Generates next random coordinates in a circle using rejection sampling
+    /// </summary>
+    /// <param name="radius">The radius of the circle</param>
+    /// <returns>A random point within a circle of radius centered at 0, 0</returns>
+    public Vector2 RandomInCircle(float radius) {
+        float x = RandomFloatInRange(-radius, radius);
+        float y = RandomFloatInRange(-radius, radius);
+        float r2 = radius * radius;
+
+        while(x * x + y * y > r2) {
+            x = RandomFloatInRange(-radius, radius);
+            y = RandomFloatInRange(-radius, radius);
+        }
+
+        return new Vector2(x, y);
+    }
+
+}
